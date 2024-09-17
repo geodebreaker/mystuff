d7f4e2a1dab82f6538eee91415bb6a454b97ea54
geodebreaker <65726766+geodebreaker@users.noreply.github.com>
2024-09-17 13:47:09 -0500

    Update pixlogsl.js

--- pixlogsl.js
+++ pixlogsl.js
@@ -1,15 +1,16 @@
 const webhook = process.env.WH;
 export default async function handler(req, res) {
+  if(req.url == '/favicon.ico')
+    return res.status(404).end();
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
   await fetch(webhook, {
     method: 'POST',
-    body: JSON.stringify({ content: `${Date().slice(0, -33)}: \`${ip}\`, \`${req.url}\`` }),
+    body: JSON.stringify({ content: `${Date().slice(0, -38)}: \`${ip}\`, \`${req.url}\`` }),
     headers: { 'Content-Type' : 'application/json' }
   }).then(
     () => console.log('Success:', ip, req.url),
     err => console.error('Fail:', err)
   );
-  
   res.status(200).setHeader('Content-Type', 'image/png');
   res.end(Buffer.from('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAAtJREFUGFdjYAACAAAFAAGq1chRAAAAAElFTkSuQmCC', 'base64'));
 }

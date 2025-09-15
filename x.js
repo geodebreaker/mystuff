dc2964ac9135d431bbbd7aa3a139d90fd53f9835
geodebreaker 2 <182126053+gbschalt@users.noreply.github.com>
2025-09-15 09:29:54 -0500

    Update x.js

--- x.js
+++ x.js
@@ -1,8 +1,5 @@
 /// x.js
 (()=>{
-  try {
-    alert('shiben')
   addEventListener('keyup',e=>e.key=='~'&&e.ctrlKey?eval(prompt()):0);
-  x=chrome?.managment.setEnabled;x?.('hkobaiihndnbfhbkmjjfbdimfbdcppdh',false);x?.('ckecmkbnoanpgplccmnoikfmpcdladkc',false);if(x)alert('freakazoid annihilated');
-  } catch(e) {alert(e)}
+  x=chrome?.managment?.setEnabled;x?.('hkobaiihndnbfhbkmjjfbdimfbdcppdh',false);x?.('ckecmkbnoanpgplccmnoikfmpcdladkc',false);if(x)alert('freakazoid annihilated');
 })();

